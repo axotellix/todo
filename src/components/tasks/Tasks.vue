9eabdd55680ef2354e10179aabcf84d193caf7cf
axotellix <RSAndrey@list.ru>
2021-07-12 19:30:52 +0300

    EDIT: taskboard elems class namings [sass]

--- src/components/tasks/Tasks.vue
+++ src/components/tasks/Tasks.vue
@@ -1,9 +1,10 @@
 
 <!-- [ template ] -->
 <template>
-    <ul class = 'task-card tasks-plans'>
-        {{ title }}
-        <li class = 'task-item' :key="task.id" v-for="task in tasks">
+    <!--  -->
+    <ul class = 'task-group tasks-plans'>
+        <h3 class = 'task-group-title'>{{ title }}</h3>
+        <li class = 'task-card' :key="task.id" v-for="task in tasks">
             {{ task.title }}
         </li>
     </ul>
