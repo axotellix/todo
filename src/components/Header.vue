a70b30b2a03db44ea1b7bcfdef870c9abacb01b3
axotellix <RSAndrey@list.ru>
2021-07-13 14:07:02 +0300

    Added > theme switcher

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,7 +10,9 @@
 
         <!-- [ dark / light theme switcher ] -->
         <div class="theme-controller">
-
+            <a @click="setTheme('night')" class = 'night active'>night</a> 
+            <span>/</span>
+            <a @click="setTheme('light')" class='light'>light</a>
         </div>
     </header>
 </template>
@@ -22,6 +24,22 @@ export default {
     name: 'Header',
     components: {
     
+    },
+    methods: {
+        setTheme( theme ) {
+            if( theme === 'night' ) {
+                document.querySelector('.light').classList.remove('active');
+                document.querySelector('.night').classList.add('active');
+                document.body.classList.remove('light');
+                document.body.classList.add('night');
+            }
+            if( theme === 'light' ) {
+                document.querySelector('.night').classList.remove('active');
+                document.querySelector('.light').classList.add('active');
+                document.body.classList.remove('night');
+                document.body.classList.add('light');
+            }
+        }
     }
 }
 </script>
